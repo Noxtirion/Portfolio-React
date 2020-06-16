03752a5753654ebb7c244ddb5509a09f7fb7d5de
Darek <dardman88@gmail.com>
2020-06-16 11:26:38 +0200

    Create basic header component and home page

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./header.styles.scss";
+
+const Header = () => (
+   <div className="header">
+      <div className="header__contact">
+         <div className="header__name">Dariusz Bartyzel</div>
+         <div className="header__contact-icons">
+            <a href="https://github.com/Noxtirion" className="header__contact-link">
+               <img src={require("../../assets/github.svg")} alt="github link" />
+            </a>
+            <a href="/" className="header__contact-link">
+               <img src={require("../../assets/linkedin.svg")} alt="linkedin link" />
+            </a>
+            <a href="mailto:dariusz.bartyzel88@gmail.com" className="header__contact-link">
+               <img src={require("../../assets/mail.svg")} alt="email" />
+            </a>
+         </div>
+      </div>
+      <div className="menu">
+         <Link className="menu__option" to="/">
+            START
+         </Link>
+         <Link className="menu__option" to="/about">
+            ABOUT ME
+         </Link>
+         <Link className="menu__option" to="/technologies">
+            TECHNOLOGIES
+         </Link>
+         <Link className="menu__option" to="/projects">
+            PROJECTS
+         </Link>
+      </div>
+   </div>
+);
+
+export default Header;
