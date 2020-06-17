6ad8461b9e1961d291727e0786b0c4dfd560b7fe
Darek <dardman88@gmail.com>
2020-06-17 12:33:01 +0200

    Add rotate to project element image

--- src/components/project-element/project-element.component.jsx
+++ src/components/project-element/project-element.component.jsx
@@ -5,15 +5,30 @@ import "./project-element.styles.scss";
 const ProjectElement = ({ item: { id, title, description, imageUrl, liveUrl, githubUrl } }) => (
    <div className="project-element">
       <div className="project-element__image">
-         <img src={require("../../assets/projects-images/" + imageUrl)} alt="project screenshot" />
-         <div className="project-element__links">
-            <a href={githubUrl}>Github</a>
-            <a href={liveUrl}>LIVE</a>
+         <div className="project-element__card">
+            <div className="project-element__front face">
+               <img
+                  src={require("../../assets/projects-images/" + imageUrl)}
+                  alt="project screenshot"
+               />
+            </div>
+            <div className="project-element__back face">
+               <div className="project-element__back-line"></div>
+            </div>
          </div>
       </div>
+
       <div className="project-element__info">
          <div className="project-element__title">{title}</div>
          <div className="project-element__description">{description}</div>
+         <div className="project-element__links">
+            <a href={githubUrl}>
+               <div className="project-element__github">Github</div>
+            </a>
+            <a href={liveUrl}>
+               <div className="project-element__live">LIVE</div>
+            </a>
+         </div>
       </div>
    </div>
 );
