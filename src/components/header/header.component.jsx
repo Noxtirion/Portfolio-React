dfb4ff65e441e4a505cf253a6535c0780ecb07f5
Darek <dardman88@gmail.com>
2020-06-18 20:41:34 +0200

    Add background to homepage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,21 +6,26 @@ import "./header.styles.scss";
 
 const Header = ({ fold }) => (
    <header className="header">
-      <div className="header__contact">
-         <div className="header__name">
-            <div className="header__first-name">Dariusz</div>{" "}
-            <div className="header__last-name">Bartyzel</div>
-         </div>
-         <div className="header__contact-icons">
-            <a href="https://github.com/Noxtirion" className="header__contact-link">
-               <img src={require("../../assets/header-icons/github.svg")} alt="github link" />
-            </a>
-            <a href="/" className="header__contact-link">
-               <img src={require("../../assets/header-icons/linkedin.svg")} alt="linkedin link" />
-            </a>
-            <a href="mailto:dariusz.bartyzel88@gmail.com" className="header__contact-link">
-               <img src={require("../../assets/header-icons/mail.svg")} alt="email" />
-            </a>
+      <div className="header__wrapper">
+         <div className="header__contact">
+            <div className="header__name">
+               <div className="header__first-name">Dariusz</div>{" "}
+               <div className="header__last-name">Bartyzel</div>
+            </div>
+            <div className="header__contact-icons">
+               <a href="https://github.com/Noxtirion" className="header__contact-link">
+                  <img src={require("../../assets/header-icons/github.svg")} alt="github link" />
+               </a>
+               <a href="/" className="header__contact-link">
+                  <img
+                     src={require("../../assets/header-icons/linkedin.svg")}
+                     alt="linkedin link"
+                  />
+               </a>
+               <a href="mailto:dariusz.bartyzel88@gmail.com" className="header__contact-link">
+                  <img src={require("../../assets/header-icons/mail.svg")} alt="email" />
+               </a>
+            </div>
          </div>
       </div>
       <MenuContextProvider>
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -4,10 +4,21 @@ import "./home.styles.scss";
 
 const Home = () => (
    <section className="home">
-      <div className="home__title">
-         <div>Hello, I'm Dariusz Bartyzel</div>
-         <br />
-         <div>Beginner Front-end Developer</div>
+      <div className="home__wrapper">
+         <img
+            src={require("../../assets/style-images/gear1.svg")}
+            className="home__gear"
+            alt="gear"
+         />
+         <div className="home__title">
+            <div>*Hello, I'm Dariusz*</div>
+            <div>Beginner Front-end Developer</div>
+         </div>
+         <img
+            src={require("../../assets/style-images/gear1.svg")}
+            className="home__gear"
+            alt="gear"
+         />
       </div>
    </section>
 );
